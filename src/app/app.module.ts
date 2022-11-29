2b72ac3a84c320585743045a2a105f1e35aa1d17
akhil <akhil.paul@ymail.com>
2022-11-29 08:56:01 +0530

    Adds news

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NewsViewComponent } from './news-view/news-view.component';
+
+const myRoute:Routes=[{path:"",component:NewsViewComponent}]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewsViewComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule,
+    RouterModule.forRoot(myRoute),
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
